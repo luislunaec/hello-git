3c4be852929bf991f3b9bd45eb282d05cf647230
Luis <luisandreluna01@hotmail.com>
2025-11-29 22:37:13 -0500

    Multiplicacion de los Digitos

--- hellogit.py
+++ hellogit.py
@@ -3,5 +3,16 @@ print("Hello Git")
 
 A=int(input("Cual es su primer numero?"))
 B=int(input("Cual es su segundo numero?"))
+
+
 suma=A+B
 print(suma)
+
+R= input("Quieres que se multipliquen los mismos datos? ")
+
+if R.lower() == "si":
+    print("La multiplicacion es: ",A*B)
+else:
+    print("No se realizara la multiplicacion")
+
+
